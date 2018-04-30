882873447baa7280a8e00364da0e9e2b8c5dfbcb
Dimitrios <tsiridakis@yahoo.gr>
2018-04-30 19:36:47 +0300

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
-const path = require('path');
-const express = require('express');
-const socketIO = require('socket.io');
-const http = require('http');
+const path              = require('path');
+const express           = require('express');
+const socketIO          = require('socket.io');
+const http              = require('http');
+
+const {generateMessage} = require('./utils/message');
 
 //We  shorten the path with the above method
-const publicPath = path.join(__dirname + '/../public');
-const port = process.env.PORT || 3000;
-const app = express();
+const publicPath        = path.join(__dirname + '/../public');
+const port              = process.env.PORT || 3000;
+const app               = express();
 
-const server = http.createServer(app); //Create a server
-const io = socketIO(server); // We pass the server that we want to use.
+const server            = http.createServer(app); //Create a server
+const io                = socketIO(server); // We pass the server that we want to use.
 
 app.use(express.static(publicPath));
 
@@ -18,16 +20,10 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
-	//socket.emit from: Admin text: Welcome to the chat app
-	socket.emit('newMessage', {
-		from: 'Admin',
-		text: 'Welcom to the Chat app'
-	});
-	//socket.broadcast.emit from: Admin text: New user joint
-	socket.broadcast.emit('newMessage', {
-		from: 'Admin',
-		text: 'New User JOINT'
-	});
+	// //socket.emit from: Admin text: Welcome to the chat app
+	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat app'));
+	// //socket.broadcast.emit from: Admin text: New user joint
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
 // Register a CUSTOM "emit event" to send to our client
  	// socket.emit('newMessage', {
@@ -41,11 +37,7 @@ io.on('connection', (socket) => {
 		console.log('createMessage', message);
 //Here the Server listen's everyone connection.
 //Let's now send to every connection the data ALSO AND SAME CLIENT WHO CREATES IT
-		io.emit('newMessage', {
-			from: message.from,
-			text: message.text,
-			createdAt: new Date().getTime()
-		});
+		io.emit('newMessage', generateMessage(message.from, message.text));
 //Let's now send to every connection the data WITHOUT CLIENT WHO CREATES IT!
 		// socket.broadcast.emit('newMessage', {
 		// 	from: message.from,
